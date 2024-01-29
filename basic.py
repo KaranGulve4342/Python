69776652ec631cc8ee4d167c6b982fde9db558dc
KaranGulve4342 <karangulve4342@gmail.com>
2024-01-30 01:33:52 +0530

    First commit

---
+++ basic.py
@@ -0,0 +1,34 @@
+# exp1 = eval(input("Enter any Equation here : "));
+# print(exp1);
+
+# TypeCasting in Python
+
+# a = "123"
+# b = 1.23
+
+# print(type(a))
+# print(type(b))
+
+# c = int(a) + b
+# print(c)
+# print(type(c))
+
+
+
+# c = 4
+# d = 5
+# # swapping in python
+# c, d = d , c 
+# print(c)
+# print(d)
+
+a = 16
+b = 5
+
+# floor division in python
+d = a // b
+print(d)
+
+# // exponentiation in python
+c = a ** b
+print(c)
