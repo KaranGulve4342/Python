3c44f4197c0f33631641350496ff3a61f297d262
KaranGulve4342 <karangulve4342@gmail.com>
2024-02-03 19:32:24 +0530

    commited changes

---
+++ dictionary.py
@@ -0,0 +1,17 @@
+Employee_Data = {"name" : "John", "age" : 24, "gender":"male"}
+# print(Employee_Data["gender"])
+
+# Iteration in Dictionary
+# using value function
+# for x in Employee_Data.values():
+#     print(x)
+
+# printing all the value name one by one
+# for x in Employee_Data:
+#     print(Employee_Data[x])
+
+
+# using items function
+# for x,y in Employee_Data.items():
+#     print(x,"->",y)
+
