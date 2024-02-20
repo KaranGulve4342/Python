1c290e37f760e514c81cd06d21a99e90cbc24ea1
KaranGulve4342 <karangulve4342@gmail.com>
2024-02-20 19:51:27 +0530

    Commited changes

---
+++ inBuiltModules.py
@@ -0,0 +1,40 @@
+# import datetime
+# x = datetime.datetime.now()
+# print(x)
+# y = datetime.datetime(2004, 6, 23)
+# print(y.strftime("%A"))
+# print(y.strftime("%a"))
+# print(y.strftime("%m"))
+# print(y.strftime("%B"))
+# print(y.strftime("%Y"))
+# print(y.strftime("%p"))
+
+# import random
+# l = ["Heads", "tail"]
+# # x = random.randint(1, 10)
+# x = random.choice(l)
+# print(x)
+
+import math
+x = max(13, 54, 99)
+y = min(13, 54, 99)
+print(x, y)
+
+p = math.pow(2, 11)
+q = pow(2, 11)
+print(p, q)
+
+b = math.sqrt(1024)
+print(b)
+
+c = abs(-1019)
+print(c)
+
+k = math.ceil(2.4)
+print(k)
+
+j = math.ceil(2.7)
+print(j)
+
+i = math.floor(3.8)
+print(i)
